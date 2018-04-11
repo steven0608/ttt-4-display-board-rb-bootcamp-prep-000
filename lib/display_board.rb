1be1cdeb68a4427626f3e256ddceb60daa79f97e
Steven Chen <ysteven.chen2@gmail.com>
2018-04-11 21:55:12 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@
 # out the current state.
 board = [" "," "," "," "," "," "," "," "," "]
 def display_board(board)
-row=[" #{board[0]} |  #{board[1]} |  #{board[2]} ","-----------"," #{board[3]} |  #{board[4]} |  #{board[5]} ","-----------"," #{board[6]} |  #{board[7]} |  #{board[8]} "]
+row=[" #{board[0]} | #{board[1]} | #{board[2]} ","-----------"," #{board[3]} | #{board[4]} | #{board[5]} ","-----------"," #{board[6]} | #{board[7]} | #{board[8]} "]
 puts row[0]
 puts row[1]
 puts row[2]
